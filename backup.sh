57f0a374c31b3070d70e376dbfcca4d3784c7c8a
wijhenke <56547835+wijhenke@users.noreply.github.com>
2020-11-13 18:14:45 +0100

    Update backup.sh
    Fixed a typo

--- backup.sh
+++ backup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 # rsync command to backup home drive recursively to /mnt/nvme1n1p1
-# while copying deleted files to /mnt/nvme1n1p1/oldbackupfilesdirs
-# each oldbackupfilesdirs entry is in a timestamped directory
+# while copying deleted files to /mnt/nvme1n1p1/oldbackups
+# each oldbackups entry is in a timestamped directory
 #
 # For daily execution create a symlink to the script in cron.daily:
 # sudo ln -s /home/rob/Files/Scripts/backup.sh backup
@@ -12,15 +12,12 @@
 #
 
 # set some variables
-
 OLDBACKUPS="/mnt/nvme1n1p1/oldbackups"
 BACKUPS="/mnt/nvme1n1p1/backup"
 SCRIPTS="/home/rob/Files/Scripts"
 
 # sync it
 sudo rsync -a -v --progress --backup-dir=$OLDBACKUPS/`date +%Y-%m-%d_%H.%M` --delete -b -s --include-from $SCRIPTS/backupinclude.txt --exclude-from $SCRIPTS/backupexclude.txt /home/rob $BACKUPS 2>$SCRIPTS/backup-errors.log
-
-#udo rsync -a -v --progress --backup-dir=/mnt/nvme1n1p1/oldbackups/`date +%Y-%m-%d_%H.%M` --delete -b -s --include-from '/home/rob/Files/Scripts/#ackupinclude.txt' --exclude-from '/home/rob/Files/Scripts/backupexclude.txt' /home/rob /mnt/nvme1n1p1/backup 2>/home/rob/Files/Scripts/backup-#rrors.log
 
 
 # delete backup dirs older then n days (where n = n+1 so 
