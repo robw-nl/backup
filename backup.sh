8ed73d92a09a97bb5e3794eb25349af3a213eb3b
robwijhenke <56547835+wijhenke@users.noreply.github.com>
2023-03-09 17:13:42 +0100

    Update backup.sh
    Various improvements
    - added some variables
    - cleaned up the code

--- backup.sh
+++ backup.sh
@@ -8,17 +8,17 @@
 # sudo ln -s /home/rob/Files/Scripts/backup.sh backup
 #
 # created by rob wijhenke november 2020
-# v1.0 rev-e
+# v1.1 rev-a march 9 2023
 #
 
 # set some variables
-OLDBACKUPS="/mnt/nvme1n1p1/oldbackups"
-BACKUPS="/mnt/nvme1n1p1/backup"
+OLDBACKUPS="/mnt/nvme1n1p1/oldbackups/"
+BACKUPS="/mnt/nvme1n1p1/backup/"
 SCRIPTS="/home/rob/Files/Scripts"
+RETENTION_CYCLE=14
 
 # sync it
 sudo rsync -a -v --progress --backup-dir=$OLDBACKUPS/`date +%Y-%m-%d_%H.%M` --delete -b -s --include-from $SCRIPTS/backupinclude.txt --exclude-from $SCRIPTS/backupexclude.txt /home/rob $BACKUPS 2>$SCRIPTS/backup-errors.log
-
 
 # open log whenever an error was thrown
 if [ -s $SCRIPTS/backup-errors.log ]
@@ -28,7 +28,7 @@ else
     # delete backup dirs older then n days 
     # (where n = n+1 so 5 = 6 days counting from 0)
     
-    find $OLDBACKUPS/* -maxdepth 0 -type d -mtime +9 | xargs sudo rm -rf;2>$SCRIPTS/backup-rm-errors.log
+    find $OLDBACKUPS/* -maxdepth 0 -type d -mtime +$RETENTION_CYCLE | xargs sudo rm -rf;2>$SCRIPTS/backup-rm-errors.log
 
     if [ -s $SCRIPTS/backup-rm-errors.log ]
     then
