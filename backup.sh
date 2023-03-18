67e9578f175042dd77db0cf2b6615df80d2e1330
robwijhenke <56547835+wijhenke@users.noreply.github.com>
2023-03-18 18:16:05 +0100

    Update backup.sh
    Fixed bug deleting backups per retention schedule

--- backup.sh
+++ backup.sh
@@ -9,16 +9,17 @@
 #
 # created by rob wijhenke november 2020
 # v1.1 rev-a march 9 2023
+# v1.1 rev-b march 18 2023
 #
 
 # set some variables
 OLDBACKUPS="/mnt/nvme1n1p1/oldbackups/"
-BACKUPS="/mnt/nvme1n1p1/backup/"
+BACKUP="/mnt/nvme1n1p1/backup/"
 SCRIPTS="/home/rob/Files/Scripts"
-RETENTION_CYCLE=14
+RETENTION_CYCLE=10
 
 # sync it
-sudo rsync -a -v --progress --backup-dir=$OLDBACKUPS/`date +%Y-%m-%d_%H.%M` --delete -b -s --include-from $SCRIPTS/backupinclude.txt --exclude-from $SCRIPTS/backupexclude.txt /home/rob $BACKUPS 2>$SCRIPTS/backup-errors.log
+sudo rsync -a -v --progress --backup-dir=$OLDBACKUPS/`date +%Y-%m-%d_%H.%M` --delete -b -s --include-from $SCRIPTS/backupinclude.txt --exclude-from $SCRIPTS/backupexclude.txt /home/rob $BACKUP 2>$SCRIPTS/backup-errors.log
 
 # open log whenever an error was thrown
 if [ -s $SCRIPTS/backup-errors.log ]
@@ -27,9 +28,9 @@ then
 else
     # delete backup dirs older then n days 
     # (where n = n+1 so 5 = 6 days counting from 0)
+    find $OLDBACKUPS/* -type d -ctime +$RETENTION_CYCLE -exec sudo rm -rf {} \;
     
-    find $OLDBACKUPS/* -maxdepth 0 -type d -mtime +$RETENTION_CYCLE | xargs sudo rm -rf;2>$SCRIPTS/backup-rm-errors.log
-
+    # open log whenever an error was thrown
     if [ -s $SCRIPTS/backup-rm-errors.log ]
     then
         kate $SCRIPTS/backup-rm-errors.log
