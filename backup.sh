6dd1c7295ec0e725a43428877d9d624daef62e95
robwijhenke <56547835+wijhenke@users.noreply.github.com>
2023-03-19 12:11:42 +0100

    Update backup.sh
    Bug fix and some improvements

--- backup.sh
+++ backup.sh
@@ -9,31 +9,36 @@
 #
 # created by rob wijhenke november 2020
 # v1.1 rev-a march 9 2023
-# v1.1 rev-b march 18 2023
+# v1.1 rev-b march 19 2023
 #
 
 # set some variables
 OLDBACKUPS="/mnt/nvme1n1p1/oldbackups/"
+SOURCE="/"
 BACKUP="/mnt/nvme1n1p1/backup/"
 SCRIPTS="/home/rob/Files/Scripts"
-RETENTION_CYCLE=10
+RETENTION_CYCLE=5
+DATE=$(date +%Y-%m-%d_%H.%M)
+BACKUP_ERRORS_LOG="$SCRIPTS/backup-errors-$DATE.log"
 
 # sync it
-sudo rsync -a -v --progress --backup-dir=$OLDBACKUPS/`date +%Y-%m-%d_%H.%M` --delete -b -s --include-from $SCRIPTS/backupinclude.txt --exclude-from $SCRIPTS/backupexclude.txt /home/rob $BACKUP 2>$SCRIPTS/backup-errors.log
+sudo rsync -a -v --progress --backup-dir="$OLDBACKUPS/$DATE" --delete -b -s --include-from "$SCRIPTS/backupinclude.txt" --exclude-from "$SCRIPTS/backupexclude.txt" $SOURCE "$BACKUP" 2>"$BACKUP_ERRORS_LOG"
 
 # open log whenever an error was thrown
-if [ -s $SCRIPTS/backup-errors.log ]
+if [ -s $BACKUP_ERRORS_LOG ]
 then
-    kate $SCRIPTS/backup-errors.log
+    kate $BACKUP_ERRORS_LOG
 else
     # delete backup dirs older then n days 
     # (where n = n+1 so 5 = 6 days counting from 0)
     find $OLDBACKUPS/* -type d -ctime +$RETENTION_CYCLE -exec sudo rm -rf {} \;
     
     # open log whenever an error was thrown
-    if [ -s $SCRIPTS/backup-rm-errors.log ]
+    if [ -s $BACKUP_ERRORS_LOG ]
     then
-        kate $SCRIPTS/backup-rm-errors.log
+        kate $BACKUP_ERRORS_LOG
+    else
+        rm $BACKUP_ERRORS_LOG
+        notify-send "Backup finished"
     fi
-    notify-send "Backup finished"
 fi
