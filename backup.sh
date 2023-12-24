117f54104b89930c38ceb44e39e1242c585756a8
robw <56547835+robw-nl@users.noreply.github.com>
2023-12-24 13:43:06 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -8,10 +8,16 @@
 # sudo ln -s /home/rob/Files/Scripts/backup.sh backup
 #
 # created by rob wijhenke november 2020
-# v1.1 rev-a march 9 2023
-# v1.1 rev-b march 19 2023
-# v1.2  December 21 2023 rewrite to introduce several functions
+# v1.0       March 6 2023 initial realease
+# v1.1 rev-a March 9 2023 fixed bug
+# v1.1 rev-b March 19 2023 cleaned up code
+# v1.2       December 21 2023 rewrite to introduce several functions
+# v1.2 rev-a December 24 2023 added error handling
 #
+
+
+# Tell bash to exit on error
+set -e
 
 # Set some variables
 OLDBACKUPS="/mnt/nvme1n1p1/oldbackups/"
@@ -24,7 +30,10 @@ BACKUP_LOG="$SCRIPTS/backup-errors-$DATE.log"
 
 # Sync files
 sync_files() {
-    sudo rsync -a -v --progress --backup-dir="$OLDBACKUPS/$DATE" --delete -b -s --include-from "$SCRIPTS/backupinclude.txt" --exclude-from "$SCRIPTS/backupexclude.txt" $SOURCE "$BACKUP" 2>"$BACKUP_LOG"
+    sudo rsync -a -v --progress --backup-dir="$OLDBACKUPS/$DATE" --delete -b -s --include-from "$SCRIPTS/backupinclude.txt" --exclude-from "$SCRIPTS/backupexclude.txt" $SOURCE "$BACKUP" 2>"$BACKUP_LOG" || {
+        echo "rsync failed with exit code $?" >&2
+        exit 1
+    }
 }
 
 # Handle log files
@@ -40,7 +49,10 @@ handle_logs() {
 
 # Delete old backup directories
 delete_old_backups() {
-    find $OLDBACKUPS/* -type d -ctime +$RETENTION_CYCLE -exec sudo rm -rf {} \;
+    find $OLDBACKUPS/* -type d -ctime +$RETENTION_CYCLE -exec sudo rm -rf {} \; || {
+        echo "Failed to delete old backups with exit code $?" >&2
+        exit 1
+    }
 }
 
 # Execute the functions
