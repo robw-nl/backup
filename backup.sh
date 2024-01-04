93a9f97fcbedb17c6db2a6a1a96fc58b8d51c264
robw <56547835+robw-nl@users.noreply.github.com>
2024-01-04 18:18:50 +0100

    Update backup.sh
    Improved error handling and on screen notifications

--- backup.sh
+++ backup.sh
@@ -13,10 +13,16 @@
 # v1.1 rev-b March 19 2023 cleaned up code
 # v1.2       December 21 2023 rewrite to introduce several functions
 # v1.2 rev-a December 24 2023 added error handling
-#
+# v1.2 rev-b January 4 2024 improved notification handling
+# v1.2 rev-b January 4 2024 added delete old backups function
+# 
 
+NOTIFICATION_DURATION=4000
 
-# Tell bash to exit on error
+# Notify that backup is starting
+notify-send -t $NOTIFICATION_DURATION "Backup is starting..."
+
+# tell bash to exit on error
 set -e
 
 # Set some variables
@@ -31,7 +37,7 @@ BACKUP_LOG="$SCRIPTS/backup-errors-$DATE.log"
 # Sync files
 sync_files() {
     sudo rsync -a -v --progress --backup-dir="$OLDBACKUPS/$DATE" --delete -b -s --include-from "$SCRIPTS/backupinclude.txt" --exclude-from "$SCRIPTS/backupexclude.txt" $SOURCE "$BACKUP" 2>"$BACKUP_LOG" || {
-        echo "rsync failed with exit code $?" >&2
+        notify-send -t $NOTIFICATION_DURATION "rsync failed with exit code $?" 
         exit 1
     }
 }
@@ -42,15 +48,16 @@ handle_logs() {
     then
         kate $BACKUP_LOG
     else
+        notify-send -t $NOTIFICATION_DURATION "No errors occurred during backup. Deleting log file."
         rm $BACKUP_LOG
-        notify-send "Backup finished"
+        notify-send -t $NOTIFICATION_DURATION "Backup finished"
     fi
 }
 
 # Delete old backup directories
 delete_old_backups() {
     find $OLDBACKUPS/* -type d -ctime +$RETENTION_CYCLE -exec sudo rm -rf {} \; || {
-        echo "Failed to delete old backups with exit code $?" >&2
+        notify-send -t $NOTIFICATION_DURATION "Failed to delete old backups with exit code $?" 
         exit 1
     }
 }
